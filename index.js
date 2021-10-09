ef79211022a9ab39aaa49c30f3843350f4f9dca9
SC ONG <proscawards99@gmail.com>
2021-10-09 14:47:33 +0800

    Update index.js

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ const isAddressValid = require("./routers/isAddressValid");
 app.use(cors());
 app.use(express.json());
 
-app.use("/latlng", latLng);
-app.use("/isaddvalid", isAddressValid);
+app.get("/latlng", latLng);
+app.get("/isaddvalid", isAddressValid);
 
 app.listen(process.env.PORT || 3000);
 
